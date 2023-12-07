1dba494164499104fa821f5ed8367c4b32b6b2fe
Ganapathi Gundapaneni <ganapathigundapaneni.2003@gmail.com>
2023-12-08 04:19:51 +0530

    Update Database.php

--- Database.php
+++ Database.php
@@ -1,7 +1,7 @@
 <?php
-$servername='localhost';
-$username='root';
-$password='';
+$servername='first-site1-server.mysql.database.azure.com';
+$username='uxthajphoz';
+$password='UJ57J14KB0P054G3$';
 $dbname = "moviebook";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
