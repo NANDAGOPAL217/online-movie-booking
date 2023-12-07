272758ebf46141594e5d9e399536f7eb4f3a83b6
Ganapathi Gundapaneni <ganapathigundapaneni.2003@gmail.com>
2023-12-08 04:57:25 +0530

    Update Database.php

--- Database.php
+++ Database.php
@@ -2,7 +2,7 @@
 $servername='first-site1-server.mysql.database.azure.com';
 $username='uxthajphoz';
 $password='UJ57J14KB0P054G3$';
-$dbname = "first-site1-database";
+$dbname = "moviebook";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
    die('Could not Connect My Sql:' .mysqli_error());
