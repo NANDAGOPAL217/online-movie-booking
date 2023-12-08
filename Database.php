b7584c7c05d21aa828fd6b2ebf7e87f9063aee50
Ganapathi Gundapaneni <ganapathigundapaneni.2003@gmail.com>
2023-12-08 07:25:29 +0530

    Update Database.php

--- Database.php
+++ Database.php
@@ -1,8 +1,8 @@
 <?php
-$servername='movie12onlineserver.mysql.database.azure.com';
-$username='gkyxuotvub';
-$password='EXK4408A102KJX4J$';
-$dbname = "movie12onlinedatabase";
+$servername='movieonline12-server.mysql.database.azure.com';
+$username='ipwrqaoryd';
+$password='23C75A46C0611880$';
+$dbname = "movieonline12-database";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
    die('Could not Connect My Sql:' .mysqli_error());
