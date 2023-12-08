3d3b8cf023134ede6fb78199ada4e4e645ad16f8
Ganapathi Gundapaneni <ganapathigundapaneni.2003@gmail.com>
2023-12-08 07:57:01 +0530

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -2,6 +2,6 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=movieonline12-server.mysql.database.azure.com;dbname=movieonline12-database", "root", "");
+$connect = new PDO("mysql:host=movieonline12-server.mysql.database.azure.com;dbname=movieonline12-database", "root", "23C75A46C0611880$");
 
 ?>
