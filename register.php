292c9d779e763bdb1b3a81d14c77d8dfbfe52d84
Ganapathi Gundapaneni <ganapathigundapaneni.2003@gmail.com>
2023-12-08 13:13:47 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,7 @@
 <?php
-include_once __DIR__ . "/Database.php";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include_once __FILE__ . "/Database.php";
 session_start();
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
