57428c888a9d1b49d7992006ab69c78a0618fcaa
Ganapathi Gundapaneni <ganapathigundapaneni.2003@gmail.com>
2023-12-08 12:06:12 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,46 +1,47 @@
 <?php
 include_once "Database.php";
 session_start();
-if (isset($_POST['submit']))
- {
- 	$username=$_POST['username'];
- 	$email=$_POST['email'];
- 	$mobile=$_POST['number'];
- 	$city=$_POST['city'];
- 	$password=$_POST['password'];
-	$filename=$_FILES['image']['name'];
-	echo $filename;
-$location='admin/image/'.$filename;
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $mobile = $_POST['number'];
+    $city = $_POST['city'];
+    $password = $_POST['password'];
+    $filename = $_FILES['image']['name'];
+    echo $filename;
+    $location = 'admin/image/' . $filename;
 
+    $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+    $file_extension = strtolower($file_extension);
+    $image_ext = array('jpg', 'png', 'jpeg', 'gif');
 
+    $response = 0;
 
-$file_extension=pathinfo($location,PATHINFO_EXTENSION);
-$file_extension=strtolower($file_extension);
-$image_ext=array('jpg','png','jpeg','gif');
+    if (in_array($file_extension, $image_ext)) {
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $location)) {
+            $response = $location;
+        }
+    }
+    echo $response;
 
-$response=0;
+    $status = 1;
+    $insert_record = mysqli_query($conn, "INSERT INTO user (`username`,`email`,`mobile`,`city`,`password`,`image`) VALUES('" . $username . "','" . $email . "','" . $mobile . "','" . $city . "','" . $password . "','" . $filename . "')");
 
-if(in_array($file_extension,$image_ext)){
-	if(move_uploaded_file($_FILES['image']['tmp_name'],$location)){
-		$response=$location;
-	}
-}
-echo $response;
+    //$query = mysqli_query($con, "SELECT ..."); // Your SQL query
 
-$status=1;
-	 if(!$conn){
-		 echo "database connection error";
-	 }
-	$insert_record=mysqli_query($conn,"INSERT INTO user (`username`,`email`,`mobile`,`city`,`password`,`image`)VALUES('".$username."','".$email."','".$mobile."','".$city."','".$password."','".$filename."')");
-	 
-	 if(!$insert_record){
-		echo "not inserted";
-	}
-	else
-	{
-		echo "hii";
-	 //echo "<script>window.location = 'login_form.php';</script>";
-	}
+    echo "i am here";
+    if (!$insert_record) {
+        die('Error in SQL query: ' . mysqli_error($conn)); // Display the SQL error if any
+    }
 
+    // Use only one if condition here
+    if (!$insert_record) {
+        echo "not inserted";
+    } else {
+        echo "i am here too";
+        echo "hii";
+        // echo "<script>window.location = 'login_form.php';</script>";
+    }
+    echo $status;
 }
 ?>
