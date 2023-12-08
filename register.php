92d170624d5dd5a278d5dc4e81bdb650ddb04fb1
Ganapathi Gundapaneni <ganapathigundapaneni.2003@gmail.com>
2023-12-08 09:03:27 +0530

    Update register.php

--- register.php
+++ register.php
@@ -31,14 +31,14 @@ echo $response;
 $status=1;
 $insert_record=mysqli_query($conn,"INSERT INTO user (`username`,`email`,`mobile`,`city`,`password`,`image`)VALUES('".$username."','".$email."','".$mobile."','".$city."','".$password."','".$filename."')");
 //$query = mysqli_query($con, "SELECT ..."); // Your SQL query
-echo "i am here"
+echo "i am here";
 if (!$inserted_record) {
     die('Error in SQL query: ' . mysqli_error($con)); // Display the SQL error if any
 }
 if(!$insert_record){
 	echo "not inserted";
 }
-echo "i am here too"
+echo "i am here too";
 else
 {
 	echo "hii";
