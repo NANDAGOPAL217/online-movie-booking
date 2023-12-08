5625d665b949f2aa26794427f827c9212ac9c43e
Ganapathi Gundapaneni <ganapathigundapaneni.2003@gmail.com>
2023-12-08 12:27:30 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 include_once "database_connection.php";
-
+$conn=mysqli_connect("newdb.mysql.database.azure.com","admin1","pass@123456","moviebook");
 session_start();
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
