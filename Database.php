c375a5dc81b232777623f570a70c1c2277971788
Ganapathi Gundapaneni <ganapathigundapaneni.2003@gmail.com>
2023-12-08 14:23:03 +0530

    Update Database.php

--- Database.php
+++ Database.php
@@ -6,5 +6,7 @@ $dbname = "moviebook";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
    die('Could not Connect My Sql:' .mysqli_error());
+}else{
+   echo "connection success";
 }
 ?>
