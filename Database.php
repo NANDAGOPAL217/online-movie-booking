de1bf2487ab484b9226c8aba4a3a420781d50380
NANDAGOPAL BHYRAVARAPU <133011182+NANDAGOPAL217@users.noreply.github.com>
2023-12-11 22:36:55 +0530

    Update Database.php

--- Database.php
+++ Database.php
@@ -6,7 +6,5 @@ $dbname = "moviebook";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
    die('Could not Connect My Sql:' .mysqli_error());
-}else{
-   echo "connection success";
 }
 ?>
