c81eb34c08e240f91830ec8a9ef1f9229a8b816d
NANDAGOPAL BHYRAVARAPU <133011182+NANDAGOPAL217@users.noreply.github.com>
2023-12-11 22:37:53 +0530

    Update login_form.php

--- login_form.php
+++ login_form.php
@@ -92,8 +92,8 @@
           if(response == 1){
                                     window.location = "index.php";
                                 }else{
-		  			error=response;
-                                     //error = " <font color='red'>!Invalid UserId.</font> ";
+		  			//error=response;
+                                     error = " <font color='red'>!Invalid UserId.</font> ";
                                      document.getElementById( "msg" ).innerHTML = error;
                                       return false;
                                 }
