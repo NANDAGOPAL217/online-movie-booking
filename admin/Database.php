97467870bf7badc1f393700bf01ef72fb1e77b2e
NANDAGOPAL BHYRAVARAPU <133011182+NANDAGOPAL217@users.noreply.github.com>
2023-12-12 02:00:50 +0530

    Update Database.php

--- admin/Database.php
+++ admin/Database.php
@@ -1,7 +1,7 @@
 <?php
-$servername='localhost';
-$username='root';
-$password='';
+$servername='newdb1.mysql.database.azure.com';
+$username='admin1';
+$password='pass@123456';
 $dbname = "moviebook";
 $conn=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$conn){
